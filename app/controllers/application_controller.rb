bcd8d51ad5345da1cfcbded9a505636d8e75b3a0
Leo Walters <leoswalters@gmail.com>
2015-08-11 19:25:43 -0700

    Additional modifications to run on Heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  
   protect_from_forgery with: :exception
+  
+  def hello
+      render text: "<h1>Hell0</h1><p>Welcome Home.</p>"
+  end
 end
